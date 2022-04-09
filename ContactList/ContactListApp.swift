cd59a2b1997b3907a0526b39a06092e9ee5da96f
mttasdemir <mt.tasdemir@gmail.com>
2022-04-09 18:16:26 +0300

    Initial Commit

---
+++ ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by Mustafa Taşdemir on 9.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
